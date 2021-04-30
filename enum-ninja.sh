fdd7423a57b984075e2d951162bfc19c239c223d
nickapic <43927184+nickapic@users.noreply.github.com>
2021-04-30 19:15:53 +0300

    Created enum-ninja.sh

---
+++ enum-ninja.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+RED="31"
+GREEN="32"
+BOLDRED="\e[1;${RED}m"
+BOLDGREEN="\e[1;${GREEN}m"
+ENDCOLOR="\e[0m"
+echo -e "----------------------------------------------------------"
+echo -e "${BOLDGREEN}EnumNinja a tiny lil script to ennumerate basic stuff in Linux for Privellege Escalation.${ENDCOLOR}"
+echo -e "${BOLDGREEN}Made by : Nickapic${ENDCOLOR}"
+echo -e "----------------------------------------------------------"
+
+if [ $1 = '-h' ]; then
+        echo "Usage: Just run the script and it should do basic enum script"
+        exit 1
+    fi
+
+varpython=`which python`
+if [ -z "$varpython" ]
+then
+    echo "${BOLDRED}Yo my dude python isnt on this box hold up lemme check for some other one!${ENDCOLOR}"
+    varpython=`which python3`
+else
+    echo -e "${BOLDGREEN}We all good here we have $varpython on this box ${ENDCOLOR}"
+fi
+
+echo "--------------------------------------------------"
+echo -e "${BOLDRED}Checking for SUID/GUID bits and printing them out${ENDCOLOR}"
+echo "--------------------------------------------------"
+find / -perm -u=s -type f 2>/dev/null
+echo "--------------------------------------------------"
+echo -e "${BOLDRED}Checking permissions on passwd/shadow files${ENDCOLOR}"
+echo "--------------------------------------------------"
+ls -la /etc/passwd
+ls -la /etc/shadow
+echo "--------------------------------------------------"
+echo -e "${BOLDRED}Checking some basic crontabs and if there are crontabs running${ENDCOLOR}"
+echo "--------------------------------------------------"
+cat /etc/crontab
+echo "--------------------------------------------------"
+echo -e "${BOLDRED}Checking what stuff is running internally on which ports${ENDCOLOR}"
+echo "--------------------------------------------------"
+netstat -aln
+echo "--------------------------------------------------"
+echo -e "${BOLDRED}Checking some basic stuff thats running using ps aux${ENDCOLOR}"
+echo "--------------------------------------------------"
+ps aux
+echo "--------------------------------------------------"
+echo -e "${BOLDRED}Checking Internal network interfaces${ENDCOLOR}"
+ifconfig
+echo -e "${BOLDRED}Checking ARP ${ENDCOLOR}"
+arp -a
+echo "--------------------------------------------------"
+
--- pingSweeper.sh
+++ pingSweeper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ $1 == "" ]
 then 
-echo "You forgot to enter the IP address you wanted to sweep"
-echo "Syntax ./scriptname.sh 192.18.1"
+echo "You forgot to enter the IP address you wanted to sweep provide the first three octates you want to scan"
+echo "Syntax ./scriptname.sh 192.18.1."
 
 else 
 for ip in `seq 1 254` : do
